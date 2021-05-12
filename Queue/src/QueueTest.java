fd39100d7c316eb8ded15164ae174d7a06438273
Prasad Viduranga <pkpviduranga@gmail.com>
2021-05-12 23:52:50 +0530

    Develop the enqueue method

--- Queue/src/QueueTest.java
+++ Queue/src/QueueTest.java
@@ -1,9 +1,17 @@
+import ds.Queue;
+
 public class QueueTest {
 
     public static void main(String[] args) {
 
+        Queue queue = new Queue();
+        queue.enqueue(100);
+        queue.enqueue(200);
+        queue.enqueue(300);
+        queue.enqueue(400);
+        queue.enqueue(500);
 
-
+        queue.print();
 
     }
 
--- Queue/src/ds/Queue.java
+++ Queue/src/ds/Queue.java
@@ -1,13 +1,48 @@
 package ds;
 
+import java.util.Arrays;
+
 public class Queue {
 
     int data;
-    int arr[]={};
+    int arr[] = {};
 
+    public void enqueue(int num) {
+        int arrTemp[] = new int[arr.length + 1];
+        for (int i = 0; i < arr.length - 1; i++) {
+            arrTemp[i] = arr[i];
+        }
+        arrTemp[arr.length] = num;
 
+    }
 
+    public void dequeue() {
 
+    }
+
+    public boolean empty() {
+        return true;
+    }
+
+    public int peek() {
+        return 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public void print() {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public boolean contains(int num) {
+        return true;
+    }
+
+    public int size() {
+        return 0;
+    }
 
 
 }
