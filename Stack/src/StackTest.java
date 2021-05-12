741b54e617e164255fe4cdc18d7a4f79bfba28c5
Prasad Viduranga <pkpviduranga@gmail.com>
2021-05-12 22:34:35 +0530

    Create push and print method

--- Stack/src/StackTest.java
+++ Stack/src/StackTest.java
@@ -1,2 +1,16 @@
+import ds.Stack;
+
 public class StackTest {
+
+    public static void main(String[] args) {
+
+        Stack stack=new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.print();
+
+    }
 }
--- Stack/src/ds/Stack.java
+++ Stack/src/ds/Stack.java
@@ -1,4 +1,47 @@
 package ds;
 
+import java.util.Arrays;
+
 public class Stack {
+    int data = 0;
+    int arr[] = {};
+
+    public void push(int num) {
+        int arrTemp[] = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arrTemp[i] = arr[i];
+        }
+        arrTemp[arrTemp.length-1] = num;
+        arr=arrTemp;
+
+    }
+
+    public void pop() {
+
+
+    }
+
+    public boolean empty() {
+
+        return true;
+    }
+
+    public int peek() {
+
+        return 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public void print() {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public boolean contains(int num) {
+        return false;
+    }
+
+
 }
